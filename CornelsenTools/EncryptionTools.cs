2c30d67df0fb517c611ddabb45f99191276ea289
lumentae <66220444+lumentae@users.noreply.github.com>
2024-09-24 22:04:12 +0200

    Initial commit

---
+++ CornelsenTools/EncryptionTools.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CornelsenTools;
+
+public class EncryptionTools
+{
+    /// <summary>
+    /// Decrypts the given byte array using the AES-128-CBC algorithm.
+    /// </summary>
+    /// <param name="toDecrypt">The byte array to decrypt.</param>
+    /// <returns>The decrypted byte array.</returns>
+    public static byte[] Decrypt(byte[] toDecrypt)
+    {
+        var api = "aes-128-cbc|D+DxJSF}2B;k-;C}";
+        var @params = api.Split('|');
+        var key = @params[1];
+        var reversedKey = ReverseString(key);
+
+        using var aes = Aes.Create();
+        aes.KeySize = 128;
+        aes.Mode = CipherMode.CBC;
+        aes.Key = Encoding.ASCII.GetBytes(key);
+
+        return aes.DecryptCbc(toDecrypt, Encoding.ASCII.GetBytes(reversedKey));
+    }
+
+    /// <summary>
+    /// Encrypts the given byte array using the AES-128-CBC algorithm.
+    /// </summary>
+    /// <param name="toEncrypt">The byte array to encrypt.</param>
+    /// <returns>The encrypted byte array.</returns>
+    public static byte[] Encrypt(byte[] toEncrypt)
+    {
+        var api = "aes-128-cbc|D+DxJSF}2B;k-;C}";
+        var @params = api.Split('|');
+        var key = @params[1];
+        var reversedKey = ReverseString(key);
+
+        using var aes = Aes.Create();
+        aes.KeySize = 128;
+        aes.Mode = CipherMode.CBC;
+        aes.Key = Encoding.ASCII.GetBytes(key);
+
+        return aes.EncryptCbc(toEncrypt, Encoding.ASCII.GetBytes(reversedKey));
+    }
+
+    private static string ReverseString(string input)
+    {
+        var charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
---
+++ CornelsenTools/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CommandLine;
+
+namespace CornelsenTools;
+
+class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var rootCommand = new RootCommand("A tool to decrypt Cornelsen files.");
+
+        var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "The file to read and decrypt.");
+        var outputOption = new Option<FileInfo?>(
+            name: "--output",
+            description: "The file to write the decrypted content to.");
+
+        var decryptCommand = new Command("decrypt", "Decrypt a file.")
+        {
+            fileOption,
+            outputOption
+        };
+        var encryptCommand = new Command("encrypt", "Decrypt a file.")
+        {
+            fileOption,
+            outputOption
+        };
+
+        decryptCommand.SetHandler((fileOptionPath, outputOptionPath) =>
+        {
+            DoEncryptionDecryption(false, fileOptionPath, outputOptionPath);
+
+            return Task.CompletedTask;
+        }, fileOption, outputOption);
+        encryptCommand.SetHandler((fileOptionPath, outputOptionPath) =>
+        {
+            DoEncryptionDecryption(true, fileOptionPath, outputOptionPath);
+
+            return Task.CompletedTask;
+        }, fileOption, outputOption);
+
+        rootCommand.AddCommand(decryptCommand);
+        rootCommand.AddCommand(encryptCommand);
+
+        await rootCommand.InvokeAsync(args);
+    }
+
+    private static void DoEncryptionDecryption(bool encrypt, FileInfo? file, FileInfo? output)
+    {
+        if (file is null or { Exists: false })
+        {
+            Console.WriteLine("File doesn't exist!");
+            return;
+        }
+
+        output ??= new FileInfo(file!.FullName + ".encrypted");
+
+        var toEncrypt = File.ReadAllBytes(file!.FullName);
+        var encrypted = encrypt ? EncryptionTools.Encrypt(toEncrypt) : EncryptionTools.Decrypt(toEncrypt);
+        File.WriteAllBytes(output!.FullName, encrypted);
+
+        Console.WriteLine($"{(encrypt ? "Encrypted" : "Decrypted")} the file successfully!");
+    }
+}
